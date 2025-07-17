1acfc64fbb7a809b74aa5d7057a52b57128355c6
karim <karim.devel@gmail.com>
2025-07-17 18:58:31 -0400

    base commit

---
+++ src/main/java/com/bank/banking/BankingSystemV2Application.java
@@ -0,0 +1,13 @@
+package com.bank.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSystemV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSystemV2Application.class, args);
+	}
+
+}
